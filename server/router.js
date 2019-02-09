c6ccfe6626a1325f8f0e9c7d6653d00e7fcde3fa
ecuyle <ecuyle@gmail.com>
2019-02-08 16:12:14 -0800

    Implements POST route to /api/reservations

--- server/router.js
+++ server/router.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     res.send('Invalid parameter. This route currently only accepts an `offering` parameter');
   } else {
     const { offering } = queryParams;
-    models.getReservationForOffering(offering)
+    models.getReservationsForOffering(offering)
       .then((response) => {
         res.send(response);
       })
@@ -20,6 +20,19 @@ router.get('/', (req, res) => {
         res.send(err);
       });
   }
+});
+
+router.post('/', (req, res) => {
+  const { body } = req;
+  models.postReservation(body)
+    .then((response) => {
+      res.statusCode = 201;
+      res.send(response);
+    })
+    .catch((err) => {
+      res.statusCode = 400;
+      res.send(err);
+    });
 });
 
 module.exports = router;
