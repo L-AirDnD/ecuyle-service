da8c80804ce1e3e0fa20aa72471d1888238f09b1
ecuyle <ecuyle@gmail.com>
2019-02-19 17:30:09 -0800

    Fix bugs relating to code blocking proxy integration

--- src/components/ReservationHook.jsx
+++ src/components/ReservationHook.jsx
@@ -18,7 +18,7 @@ const ReservationHook = () => (
         <Title4Light>It&#39;s been viewed 500+ times in the past week.</Title4Light>
       </StyledHookLeft>
       <StyledHookRight>
-        <img src="assets/lightbulb.svg" alt="lightbulb" height="85px" width="85px" />
+        <img src="https://s3.us-east-2.amazonaws.com/lairdnd-reservations/lightbulb.svg" alt="lightbulb" height="85px" width="85px" />
       </StyledHookRight>
     </TopWrapper>
   </Wrapper>
